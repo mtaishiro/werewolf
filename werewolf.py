7d4025d18ec24f7ede340357acba26b313f2de0b
Taishiro Mizutani <taimizutani@BiGMaC.local>
2020-12-21 16:42:28 +0900

    intro done

--- werewolf.py
+++ werewolf.py
@@ -1,13 +1,10 @@
 import random
 
-#the list of jobs according to the number of players
-fourplayer = ["villager", "werewolf", "knight", "fortune teller"]
-fiveplayer = ["villager", "villager", "werewolf", "knight", "fortune teller"]
-sixplayer = ["villager", "villager", "villager", "werewolf", "knight", "fortune teller"]
-
 #ask for the number of players playing the game
-playernum = input("type the number of players. It has to be between 4 and 6: ")
+playernum = input("type the number of players. It has to be between 5 and 8: ")
 
+#ask for the job and make a list out of it
+joblist = input ("type the jobs you want in the game, separated with a comma and space: ").split(", ")
 
 #ask for player name
 playerlist = []
@@ -18,4 +15,11 @@ for i in range (int(playernum)):
 #make random list from 0 to len(playernum)
 numlist = list(range(0, int(playernum)))
 rannumlist = random.sample(numlist,len(numlist))
+
+#assign player a job and store it in the list
+playerjob = {}
+
+for i in range(int(playernum)):
+    rannum = rannumlist[i]
+    playerjob[playerlist[i]] = joblist[rannum]
 
